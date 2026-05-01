8b4bceb26b79e1cfa4fd573f8858eed78012dadd
Jannatul Ferdous <ferdousmunmun75@gmail.com>
2026-05-02 04:19:16 +0600

    auth added

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -1,11 +1,20 @@
-
-
-import { Button } from "@heroui/react"
-import Link from "next/link"
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
 
 
 
 const Navber = () => {
+const userData = authClient.useSession();
+  const user = userData.data?.user;
+
+  const handleSignOut = async () => {
+    await authClient.signOut();
+
+  }
+
   return (
    <nav className="container mx-auto sticky top-0 z-40 w-full">
   <header className="flex h-16 items-center justify-between px-6">
@@ -15,10 +24,37 @@ const Navber = () => {
       <li className="font-semibold text-md text-blue-950"><Link href={"/all-photos"}>Courses</Link></li>
       <li className="font-semibold text-md text-blue-950"><Link href={"/profile"}>My Profile</Link></li>
     </ul>
-    <div className=" flex gap-2 ">
-         <Button className= "px-4 py-2 bg-green-200 text-blue-950">Login</Button>
-    <Button className= "px-4 py-2 bg-green-200 text-blue-950">Logout</Button>
-    </div>
+
+
+    <div className="flex gap-4">
+          {!user && (
+            <ul className="flex items-center  text-sm gap-5">
+              <li>
+                <Link href={"/signup"}>SignUp</Link>
+              </li>
+              <li>
+                <Link href={"/signin"}>SignIn</Link>
+              </li>
+            </ul>
+          )}
+
+          {user && (
+            <div className="flex gap-3">
+              <Avatar size="sm">
+                <Avatar.Image
+                  alt="John Doe"
+                  src={user?.image}
+                  referrerPolicy="no-referrer"
+                />
+                <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+              </Avatar>
+
+              <Button onClick={handleSignOut} size="sm" variant="danger">SignOut</Button>
+            </div>
+          )}
+        </div>
+
+
    
   </header>
 </nav>
