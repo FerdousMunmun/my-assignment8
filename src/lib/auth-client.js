d202918846730aac26cad1387f62d802a419b87b
Jannatul Ferdous <ferdousmunmun75@gmail.com>
2026-05-04 02:35:40 +0600

    link update

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,6 +3,8 @@ export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     // baseURL: "https://my-assignment8.vercel.app"
     baseURL:process.env.BETTER_AUTH_URL
+
+    //  baseURL: process.env.NEXT_PUBLIC_AUTH_URL,
 })
 
 
